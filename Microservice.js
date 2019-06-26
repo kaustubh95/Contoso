bd76a8dc2517db7d05e2fa429b1e998e62e9c2ac
dac <dac@dypm.org>
2019-06-26 08:01:01 +0530

    Third Commit

--- Microservice.js
+++ Microservice.js
@@ -1,17 +1,17 @@
 var express = require('express');
 var app = express();
 
-var ContosoController=function (req, res) {
-  var User=[
+var userController=function (req, res) {
+  var user=[
             {FName:'Ajay',LName:'kale',age:43,Utype:'Employee'},
             {FName:'Vikas',LName:'Sharma',age:36,Utype:'Employee'},
-            {FName:'Kabir',LName:'Singh',age:28,Utype:'Client'},
+            {FName:'Kabir',LName:'Singh',age:28,Utype:'Customer'},
             {FName:'Azad',LName:'Ahmed',age:25,Utype:'Employee'},
-            {FName:'Umang',LName:'Tripathi',age:23,Utype:'Client'}
-      ];
-  res.send(User);
+            {FName:'Umang',LName:'Tripathi',age:23,Utype:'Customer'}
+			];
+  res.send(user);
 };
 
-app.get('/User',ContosoController );
-var server = app.listen(8000);
-console.log("Listening At Port 8000");
+app.get('/user',userController );
+var server = app.listen(8001);
+console.log("Listening At Port 8001");
